6661354049f7fc6c76a99a183034f9633e07b774
MadeleinePiffaretti <piffaretti.madeleine@gmail.com>
2015-09-26 12:15:57 +0200

    rencontres graph + liens sites contributeurs

---
+++ js/dendo.js
@@ -0,0 +1,101 @@
+var jsonDom = {"name": "RevuePerroquet", "children":[{"name":"#0","class":"Numéro","children":[{"name":"Le temps se cristallise en cette heure dramatique","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Astrid Campion","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"son","class":"Contribution","children":[{"name":"Camille Blanès","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"ne pas vouloir en arriver au point zéro","class":"Titre","children":[{"name":"son","class":"Contribution","children":[{"name":"Elodie Fiat","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"texte","class":"Contribution","children":[{"name":"Bastien Simon","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"heure 0","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Jérémie Fischer","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Truites","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Michel Fadat","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"son","class":"Contribution","children":[{"name":"Daniel Martin-Borret","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Les ballons","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Marion Fayolle","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Les souffleurs","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Marion Fayolle","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Multiplicateur","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Vincent Broquaire","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Interminé du 20 février","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Joseph Harara","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Z…O","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Alexis Beauclair","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Ni une ni deux","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Jean Renoux","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"son","class":"Contribution","children":[{"name":"Zoé Manhès","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Sur la dérive des hommes","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Mügluck","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Double zéro","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Georgio","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"son","class":"Contribution","children":[{"name":"Elisa Monteil","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Collage n°0","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Vincent Zuanella","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"son","class":"Contribution","children":[{"name":"Mathieu Agulhon","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Bonhomme 317","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Guillaume Chauchat","class":"Contributeur","size":1,"color":null,"label":null}]}]}]},{"name":"#1","class":"Numéro","children":[{"name":"sans titre","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Pauline Barzilai","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Accidents","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Barbara Métais-Chastanier","class":"Contributeur","size":1,"color":null,"label":null},{"name":"Noëlle Renaude","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"son","class":"Contribution","children":[{"name":"Nicolas Maury","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Perrocul","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Pauline Barzilai","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Basculer","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Magali Brénon","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Le privilège ambigu de frémir","class":"Titre","children":[{"name":"image","class":"Contribution","children":[{"name":"Nicolas Tourre","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"basso ostinato","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Michel Fadat","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Kyotonomatopée","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Laurent Colomb","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Noelle et Nicolas enquetent","class":"Titre","children":[{"name":"image","class":"Contribution","children":[{"name":"Marie Vermillard","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Marcello Mastroianni dans la dolce vita","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Pauline Barzilai","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Charlotte Salomon","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Sarah Cillaire","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"image","class":"Contribution","children":[{"name":"Christine Beau","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"un musical","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Sarah Cillaire","class":"Contributeur","size":1,"color":null,"label":null}]},{"name":"image","class":"Contribution","children":[{"name":"Christine Beau","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"Routines","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Daniel Martin-Borret","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"reflets","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Anne-Lise Bertrand","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"l'absence","class":"Titre","children":[{"name":"dessin","class":"Contribution","children":[{"name":"Anne-Lise Bertrand","class":"Contributeur","size":1,"color":null,"label":null}]}]},{"name":"le théâtre des absents","class":"Titre","children":[{"name":"texte","class":"Contribution","children":[{"name":"Sébastien Porte","class":"Contributeur","size":1,"color":null,"label":null}]}]}]}]};
+
+      var diameter = 700;
+
+  		var tree = d3.layout
+			.tree()
+			.size([360, diameter / 2 - 100]);
+			
+			var diagonal = d3.svg
+			.diagonal
+			.radial()
+			.projection(function(d) {
+				return [d.y, d.x / 180 * Math.PI];
+			});
+
+			var rotate = d3.scale
+			.linear()
+			.domain([0, 1])
+			.range([0, 359]);
+
+			var col = d3.scale.category20c();
+
+			var svg = d3.select('.svg-container')
+			.append("svg").attr("width", 700)
+			.attr("height", diameter-100)
+			.append("g")
+			.attr("transform", "translate(" + 350 + "," + 275 + ")rotate(" + rotate(60) + ")");
+
+			//d3.json(jsonDom, function(error, root) {
+			var nodes = tree.nodes(jsonDom);
+			var edges = tree.links(nodes);
+
+			var link = svg.selectAll(".edge")
+			.data(edges)
+			.enter()
+			.append("path")
+			.attr("class", "edge")
+			.attr("d", diagonal)
+			.style("opacity", 0);
+
+			var node = svg.selectAll(".node")
+			.data(nodes)
+			.enter()
+			.append("g")
+			.attr("class", "node");
+
+			var yMax = d3.max(nodes, function(d) {
+					return d.y
+				});
+
+			var radiusScale = d3.scale
+			.linear()
+			.domain([0, yMax])
+			.range([7, 3]);
+
+			node.on('mouseover', function(e) {
+				var tag = this.textContent;
+				var output = document.getElementById("tag");
+				var x = d3.select(this).data()[0].x;
+				var y = d3.select(this).data()[0].y;
+
+				output.innerHTML = tag;
+				output.style.top = d3.event.layerY - 30 + "px";
+				output.style.left = d3.event.layerX + 5 + "px";
+				output.style.display = "block";
+			}).on('mouseout', function() {
+				var output = document.getElementById("tag");
+				output.style.display = "none";
+			});
+
+			node.append("circle")
+			.attr("r", function(d) {
+				return (radiusScale(d.y));
+			}).style("fill", function(d) {
+				return col(d.name);
+			});
+
+			svg.on('mouseover', function(e){
+			node.transition()
+			.duration(3000)
+			.attr("transform", function(d) {
+				return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+			});
+
+			svg.selectAll('.edge')
+			.transition()
+			.duration(4000)
+			.delay(200)
+			.style("opacity", .8);
+			});
+			
+
+			node.append("text")
+			.text(function(d) {
+				return d.name;
+			}).style({
+				"fill" : "#fff",
+				"opacity" : "0"
+			});
+
+
